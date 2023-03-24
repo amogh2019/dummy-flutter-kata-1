7cb565026f9679bc6f72ef62cac1d3852e71e572
amogh2019 <56151951+amogh2019@users.noreply.github.com>
2023-03-24 14:29:29 +0000

    chore/Generate Random Fun Sentence 1. plugging state in stateless MyApp using state change notification provider and changenotifier  and listening 2. propogating theme color to child 3. propogating theme text to child 4. using different ui widgets materialapp,scaffold, center, column, text,elevated button, card, padding 5. using existing widgets extracted to custom widgets like homeapp, bigcard

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -17,7 +19,8 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
         ),
         home: MyHomePage(),
       ),
@@ -25,28 +28,96 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// this state will store the variables/state that can be
+// listened by any child object/widget (for that we need to
+// plug a state change notifier(ChangeNotifier widget) in parent,
+// and plug a state listeners in childs(watch method) ).
+// Also have methods to change variable values
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+  var randomWordTwo = nouns.elementAt(Random().nextInt(100));
+
+  var randomPreposition = ["on", "in", "over", "through", "of", "at"]
+      .elementAt(Random().nextInt(6));
+
+  void getNextRandomWord() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var currentRandomWord = appState.current;
+    var currentRandomPreposition = appState.randomPreposition;
+
+    var theme = Theme.of(context);
 
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            color: Colors.amber[400],
-            child: Text('A random idea:'),
-          ),
-          Container(
-            color: Colors.red[300],
-            child: Text(appState.current.asLowerCase),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              // color: Colors.amber[400],
+              color: theme.colorScheme.background,
+              child: Text(
+                  'A random : ${appState.randomWordTwo} ${currentRandomPreposition}'),
+            ),
+            SizedBox(
+              height: 10, //just added to create visual gap
+            ),
+            Container(
+              color: theme.colorScheme.surface,
+              child: BigCard(currentRandomWord: currentRandomWord),
+            ),
+            SizedBox(
+              height: 10, //just added to create visual gap
+            ),
+            ElevatedButton(
+                // onPressed: () {
+                //   print('this button is pressed!');
+                // },
+                // onPressed: appState.getNextRandomWord,
+                onPressed: () {
+                  appState.getNextRandomWord();
+                },
+                child: Text('Next??'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.currentRandomWord,
+  });
+
+  final WordPair currentRandomWord;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+
+    var textStyle = theme.textTheme.displayMedium!
+        .copyWith(color: theme.colorScheme.onPrimary);
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          currentRandomWord.asSnakeCase,
+          style: textStyle,
+          semanticsLabel:
+              "${currentRandomWord.first} ${currentRandomWord.second}", // this is for accessablity //a reader reads the normal snakecase wala string // but the talkback service will pronunce this string value instead for visually challenged person
+        ),
       ),
     );
   }
